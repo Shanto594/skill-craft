06b8d47ab4525a72fbac4e5baefbd3d8296866ed
ridwanmalik <skredonemalik@gmail.com>
2023-11-07 19:32:00 +0600

    Shadcn Setup

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui"
+
+const Home = () => {
+  return (
+    <div className="text-3xl font-bold">
+      <h1>Home</h1>
+      <Button>Button</Button>
+    </div>
+  )
+}
+export default Home
