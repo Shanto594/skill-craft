1f35f1a0dbb2b78d1d5c4833b1181d7b54134f8b
ridwanmalik <skredonemalik@gmail.com>
2023-11-22 23:27:13 +0600

    Course Creation Ul

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { ToastProvider } from "@/components/providers"
 import "@/styles/globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
