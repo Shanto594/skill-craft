87f00fb260633ad2444c48a5b5bbd2d83cd8231a
ridwanmalik <skredonemalik@gmail.com>
2023-11-24 19:51:22 +0600

    Course Wip

---
+++ app/api/courses/route.js
@@ -0,0 +1,31 @@
+import { db } from "@/services"
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+// import { isTeacher } from "@/lib/teacher"
+
+export async function POST(req) {
+  try {
+    const { userId } = auth()
+    const { title } = await req.json()
+
+    if (
+      !userId
+      //  || !isTeacher(userId)
+    ) {
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    })
+
+    return NextResponse.json(course)
+  } catch (error) {
+    console.log("[COURSES]", error)
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
--- components/ui/index.js
+++ components/ui/index.js
@@ -1,5 +1,7 @@
 export * from "./button"
 export * from "./form"
+export * from "./icon-badge"
 export * from "./input"
 export * from "./label"
 export * from "./sheet"
+export * from "./textarea"
