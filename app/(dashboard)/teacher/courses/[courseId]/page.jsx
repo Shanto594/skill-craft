be573878d515572d1737bd94eed3b542ca3c3090
ridwanmalik <skredonemalik@gmail.com>
2023-11-26 23:47:25 +0600

    Course Chapter

--- app/(dashboard)/teacher/courses/[courseId]/page.jsx
+++ app/(dashboard)/teacher/courses/[courseId]/page.jsx
@@ -4,16 +4,15 @@ import { auth } from "@clerk/nextjs"
 import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react"
 import { redirect } from "next/navigation"
 
-// import { db } from "@/lib/db"
 // import { Banner } from "@/components/banner"
 
+import { AttachmentForm } from "./_components/attachment-form"
 import { CategoryForm } from "./_components/category-form"
+import { ChaptersForm } from "./_components/chapters-form"
 import { DescriptionForm } from "./_components/description-form"
 import { ImageForm } from "./_components/image-form"
-import { TitleForm } from "./_components/title-form"
 import { PriceForm } from "./_components/price-form"
-import { AttachmentForm } from "./_components/attachment-form"
-// import { ChaptersForm } from "./_components/chapters-form"
+import { TitleForm } from "./_components/title-form"
 // import { Actions } from "./_components/actions"
 
 const CourseIdPage = async ({ params }) => {
@@ -29,11 +28,11 @@ const CourseIdPage = async ({ params }) => {
       userId,
     },
     include: {
-      // chapters: {
-      //   orderBy: {
-      //     position: "asc",
-      //   },
-      // },
+      chapters: {
+        orderBy: {
+          position: "asc",
+        },
+      },
       attachments: {
         orderBy: {
           createdAt: "desc",
@@ -58,7 +57,7 @@ const CourseIdPage = async ({ params }) => {
     course.imageUrl,
     course.price,
     course.categoryId,
-    // course.chapters.some(chapter => chapter.isPublished),
+    course.chapters.some(chapter => chapter.isPublished),
   ]
 
   const totalFields = requiredFields.length
@@ -103,7 +102,7 @@ const CourseIdPage = async ({ params }) => {
                 <IconBadge icon={ListChecks} />
                 <h2 className="text-xl">Course chapters</h2>
               </div>
-              {/* <ChaptersForm initialData={course} courseId={course.id} /> */}
+              <ChaptersForm initialData={course} courseId={course.id} />
             </div>
             <div>
               <div className="flex items-center gap-x-2">
